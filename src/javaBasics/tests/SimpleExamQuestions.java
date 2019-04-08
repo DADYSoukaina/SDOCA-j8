19a031b484283fff2263e417951a7d3579ec2c82
DADYSoukaina <dadysoukaina@gmail.com>
2019-04-08 07:03:09 +0100

    test chapter 1

---
+++ src/javaBasics/tests/SimpleExamQuestions.java
@@ -0,0 +1,38 @@
+package tests;
+
+public class SimpleExamQuestions {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	// 1.1: e
+	/** 
+	 * Class EJava with default access modifiers and it's in default package 
+	 * so we cannot use it inside other packaged class.
+	 */
+	
+	// 1.2: a - b - d
+	
+	// 1.3: d ( d-f is wrong: java case sensitive)
+	
+	// 1.4: b - c
+	
+	// 1.5: d
+	
+	// 1.6: c
+	
+	// 1.7: c
+		// compiles one and executed by any JVM without recompilation 
+
+	// 1.8: b
+	
+	// 1.9: c
+	
+	// 1.10: c
+		// default class in other package
+	// 1.11: a
+	
+	 // score is 10/11
+}
