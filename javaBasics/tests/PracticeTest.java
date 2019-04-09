21dd7d93d37a7bdb7e2fea40319d1b69096f9658
DADYSoukaina <dadysoukaina@gmail.com>
2019-04-09 11:09:59 +0100

    Chapter 1: Practice test

---
+++ javaBasics/tests/PracticeTest.java
@@ -0,0 +1,116 @@
+// result is 14/17, 82% Pass
+		/**
+		 * 1. 
+		 * 9.
+		 * 14.
+		 */
+
+
+package tests;
+import static java.lang.System.*;
+
+public class PracticeTest {
+	
+// 5. 	static int x = 4;
+//	static int x=9;
+	public static void main(String[] args) {
+		
+		/**
+		 * 1. An exception: java.lang.ArrayIndexOutOfBoundException
+		 */
+//		int[] a = {1,2,3};
+//		System.out.println(a[3]);
+		
+		/**
+		 * 2. JVM: Resonsable for the hardware and operating system independence of the Java SE platform.
+		 */
+		
+		/**
+		 * 3. java source file must include "Class declaration" to compile and generate bytecode.
+		 */
+		
+		/**
+		 * 4. javac command compiles a .java file into a .class file.
+		 */
+		
+		/**
+		 * 5. print the value of static variable x=4 (montioned above) not the value of variable x=5 inside the boucle for. 
+		 */
+		
+//		for(int x=5; x<10; x++)
+//			x++;
+//		System.out.println(x--);
+		
+		/**
+		 * 6. compilation fails due to not declaring the variable y (in while) befor using it in do{}, declaration inside do{} generate a compilation error
+		 */
+		
+//		int x=0;
+//		do {
+//			int y=x++;
+//			System.out.print(y);
+//		}while(y<5);
+		
+		/**
+		 * 7. JRE (java runtime environment is sufficient to run a java program only
+		 */
+		
+		/**
+		 * 8. Package mine.whiz
+		 */
+		
+		/**
+		 * 9. Static methods introduced in interfaces enables the ability to use multiple inheritance.
+		 */
+		
+		/**
+		 * 10. for() use the varible x inside main method() an not the static one
+		 */
+		
+//		int x=8;
+//		for(;x>-1; x--) 
+//			System.out.print(x);
+		
+		/**
+		 * 11. compilation fails due to ; after for(), x not recognized.
+		 */
+		
+//		for(int x=3; x>-1; x--);
+//		System.out.print(x);
+	
+		/**
+		 * 12. comment multi lines
+		 */
+		
+		/**
+		 * 13. valid top level class declaration: class If{}, it's can't be protected or static
+		 */
+		
+		/**
+		 * 14. run without main method just by static{}
+		 */
+		
+		/**
+		 * 15. print A
+		 */
+		
+//	out.print("A");
+		
+		/**
+		 * 16. import static
+		 */
+		
+		/**
+		 * All of the above
+		 */
+	
+	
+	}
+	
+//	static {
+//		System.out.println("run ok");
+//		System.exit(0);
+//	}
+	
+
+}
