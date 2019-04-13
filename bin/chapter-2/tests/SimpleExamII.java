4aa5450ccbedc505e90f8a4703ca904327a0fbf8
DADYSoukaina <dadysoukaina@gmail.com>
2019-04-13 20:46:04 +0100

    Simple exam II

---
+++ bin/chapter-2/tests/SimpleExamII.java
@@ -0,0 +1,110 @@
+package tests;
+	// Result: 9/10
+public class SimpleExamII {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		/**
+		 * 1. a add to char values executed correctly int to char is pernited
+		 *    int -> float up casting automaticly 
+		 */
+//			
+//			int a = 20;
+//			int b = 30;
+//			
+//			System.out.print((char)a +(char)b);
+//			
+//			int age = 20;
+//			System.out.print("  ");
+//			System.out.println((float)age);
+		
+			/**
+			 * 2.answers 4, 6, 7 / a,c,d
+			 */
+			
+//			char aa = 'a';
+//			System.out.println(++aa);
+//
+//			char bb = -10;
+//			System.out.println(++aa + bb + d);
+//			
+//			char cc = '1';
+//			integer d = 1000;
+			
+			/**
+			 * 3.  611
+			 */ 
+		
+//			int aaa=10;
+//			long bbb = 20;
+//			short ccc = 30;
+//				System.out.println(++aaa + bbb++ * ccc);
+			
+			/**
+			 * 4.  	d
+			 */
+				
+//			Boolean buy = new Boolean(true);
+//			Boolean sell = new Boolean(true);
+//			System.out.print(buy == sell);
+//			boolean buyPrim = buy.booleanValue();
+//			System.out.print(!buyPrim);
+//			System.out.print(buy && sell );
+		
+		
+		/**
+		 * 5. c,d,e,f,g  / wrong e to store decimal not integer shoud add d at the end of the number
+		 */
+				
+//		bit a = 0;
+//		integer a2 = 7;
+//	long a3 = 0x10C;
+//	short a4 = 0512;
+//	double a5 = 10;
+//	byte a7 = -0;
+//	long a8 = 123456789;
+	
+		
+		/**
+		 * 6. the rest of devision: 51/20  ctrl+= 1 and ++ctrl
+		 */
+//		int ctr = 50;
+//		System.out.println(++ctr % 20 );
+		
+		/**
+		 * 7. d
+		 */
+		
+		/**
+		 * 8. d
+		 */
+		
+//		boolean b =false;
+//		int i = 90;
+//		System.out.print(i>=b)
+
+		/**
+		 * 9. true  b, c 
+		 * 
+		 */
+		
+//		int num1 =12;
+//		float num2 = 17.8f;
+//		boolean eJavaResult = true;
+//		boolean returnVal = num1 >= 12 && num2 < 4.567 || eJavaResult== true;
+//		System.out.println(returnVal);
+//		
+	
+	/**
+	 * 10. 10.0 false
+	 */
+
+//		boolean myBool = false;
+//		int yourInt = 10;
+//		float myFloat=19.54f;
+//		System.out.println(myFloat = yourInt);
+//		System.out.print(myBool = false);
+		
+	}
+}
