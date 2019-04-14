a6c45f7114134a6aa8b217924e907af25bb14f5e
DADYSoukaina <dadysoukaina@gmail.com>
2019-04-14 12:48:20 +0100

    Section test II

---
+++ bin/chapter-2/tests/SectionTestII.java
@@ -0,0 +1,98 @@
+package tests;
+
+public class SectionTestII {
+
+	int y=10;
+	int j=10;
+	static Integer i;
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		/**
+		 * 1.  e: none of the above
+		 */
+		
+//		boolean b = tr_ue;
+//		double d =0._42;
+//		long l = 1000_l;
+//		int i=_1000;
+		
+		/**
+		 * 2. 
+		 */
+		
+		
+		
+		
+		/**
+		 * 5. to use the first y above it's should be static; the second y declarated but not initialized
+		 */
+		
+//		int y;
+//		final int x;
+//		x=10;
+//		System.out.print(x*y);
+		
+		
+		/**
+		 * 7. Exception null pointer can't be initialired inside main
+		 */
+	
+//		Double j = 0.25;
+//		Double z = j+i;
+//		System.out.print(z);
+		
+		
+		/**
+		 * 10. can't convert from int to short 
+		 */
+		
+//		short s=10;
+//		s+=10;
+//		s++;
+//		s=s+1;
+//		System.out.print(s);
+
+		
+		/**
+		 * 11. local variable is not initilized.
+		 */
+		
+//		int j;
+//		if( new SectionTestII().go(10))j=5;
+//		System.out.print(j);
+//
+//		
+//		String a="a";
+//		
+		
+		
+		/**
+		 * 14. 
+		 * - it's illegal to access instance variables using class name (compile time error),
+		 *  to access them we have to access it through valid object reference.
+		 * - Using the object referance we can access instance variables.
+		 * - we can access static variables using clas name
+		 * - also we can access static variables using object reference.
+		 *   
+		 * 
+		 */
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+	
+//	boolean go(int y) {
+//		if(y>5)return true;
+//		else return false;
+//	}
+
+}
